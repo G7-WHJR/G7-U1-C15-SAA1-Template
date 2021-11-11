7ed2f3400723af13b2fce57f3c1268026e2b0765
unknown <raveen.anand@whitehatjr.com>
2021-11-11 17:30:55 +0530

    v2

--- FlashLight/sketch.js
+++ FlashLight/sketch.js
@@ -2,21 +2,14 @@ var currentColor;
 var colors=["white","black"];
 
 function setup() {
-    //size of the canvas to draw on
     createCanvas(innerWidth,innerHeight); 
-
-    // Set the background to white, and move the line to setup();
     background("black");
-     
 }
 
 function draw()
 {
-
-  let x = 0;
+  var x = 0;
     for (i = 0; i < colors.length; i++){
-
-       //make an object for each color box:
         var currentColor = new ColorButton(x, 0, 100, 50, colors[i] );  
         currentColor.appear();
         x = x + 100;
@@ -24,21 +17,12 @@ function draw()
 
   if(mouseIsPressed)
   {
-    if () {
-      currentColor = "white";
-    
-    } else if () {
-      currentColor = "black";
-    
-    } 
-    
+    //add if else if conditions :
+
     changeBackground(currentColor);
   }
-
 }
 
 function changeBackground(currentColor){
   background(currentColor);
 }
-
-
